f8d5b92771a996394605bf8f7333c7a4b38329c1
root <root@Candycheeto.(none)>
2016-05-05 19:50:47 -0400

    Switch to SocketIO

--- index.js
+++ index.js
@@ -3,29 +3,32 @@ var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
 
-server.listen(3000, function () {
-  console.log('Server listening at port 3000');
+server.listen(5000, function () {
+  console.log('Server listening at port 5000');
 });
 
 // This will serve the static files from earlier
 app.use(express.static(__dirname + '/static'));
 
 io.on('connection', function (socket) {
-  console.log("New connection");
+  console.log("New connection:\t" + socket.id);
 
   socket.on('join room', function (roomId) {
-    console.log("Joined " + roomId);
-
     socket.join(roomId);
   });
 
   socket.on('new message', function (data) {
     console.log(data);
-    var room = socket.rooms[1];
 
-    socket.broadcast.to(room).emit('new message', {
-      message: data
-    });
+    var message = {
+      text: data.text,
+      name: data.name,
+      uniqueId: Math.round(Math.random() * 10000),
+      date: new Date(2016, 3, 14, 13, 0),
+      pkey: data.pkey
+    };
+
+    socket.broadcast.emit('new message', message);
   });
 
 });
