7cc0f9a4ec3fe1045dce49b2154aa1e6970ee165
Tristan Hari <kptristan@10-249-33-102.wireless.oregonstate.edu>
2016-06-02 21:40:28 -0700

    committing shitty changes

--- db.js
+++ db.js
@@ -3,42 +3,78 @@ require('dotenv').config();
 var crypto = require('crypto');
 var pg     = require('pg');
 
-var client = new pg.Client(process.env.DATABASE_URL);
-client.connect();
+//var client = new pg.Client(process.env.DATABASE_URL);
+var client = new pg.Client("postgres://localhost:5432/homiedb");
+//client.connect();
 
-function create_message(msg, cb) {
-  var sql = 'INSERT INTO messages (room_id, name, body) VALUES ($1, $2) RETURNING *';
-  client.query(sql, [msg.room_id, msg.name, msg.text], (err, results) => {
-    if (err) throw err;
-    cb(results.rows[0]); 
+pg.connect("postgres://localhost:5432/homiedb", function(err, client, done) {
+  if(err) {
+    return console.error('error fetching client from pool', err);
+  }
+  client.query('SELECT $1::int AS number', ['1'], function(err, result) {
+    //call `done()` to release the client back to the pool
+    done();
+
+    if(err) {
+      return console.error('error running query', err);
+    }
+    console.log(result.rows[0].number);
+    //output: 1
+
   });
-}
+  client.query('SELECT * FROM messages;', function(err, result) {
+    //call `done()` to release the client back to the pool
+    done();
 
-function create_room(name, cb) {
-  var sql = 'INSERT INTO rooms (name, aes_key) VALUES ($1, $2) RETURNING *'; 
-  var key = generate_aes_key(); 
-  client.query(sql, [name, key.toString()], (err, results) => {
-    if (err) throw err;
-    cb(results.rows[0]); 
+    if(err) {
+      return console.error('error running query', err);
+    }
+    console.log(result.rows);
+    //output: 1
+
   });
-}
+});
 
-function find_room(name, cb) {
-  var sql = "SELECT * FROM rooms WHERE name = $1"; 
-  client.query(sql, [name], (err, results) => {
-    if (err) throw err;
-    cb(results.rows[0]); 
-  });
-}
+var dbfun = module.exports = {
 
-function find_messages(room_id, cb) {
-  var sql = 'SELECT * FROM messages WHERE room_id = $1';
-  client.query(sql, [room_id], (err, results) => {
-    if (err) throw err;
-    cb(results.rows); 
-  });
-}
+  create_message: function(msg, cb) {
+    var sql = "INSERT INTO messages (room_id, name, body) VALUES ("+ msg.roomId +", "+ msg.name +", "+ msg.text +");";
+    console.log(sql);
+    client.query(sql, [msg.room_id, msg.name, msg.text], (err, results) => {
+      if (err) throw err;
+      cb(results.rows[0]);
 
-function generate_aes_key() {
-  return crypto.randomBytes(32).toString('hex');
+    });
+  },
+
+  create_room: function(name, cb) {
+    var sql = 'INSERT INTO rooms (name, aes_key) VALUES ($1, $2) RETURNING *';
+    var key = generate_aes_key();
+    client.query(sql, [name, key.toString()], (err, results) => {
+      if (err) throw err;
+      cb(results.rows[0]);
+    });
+  },
+
+  find_room: function(name, cb) {
+    var sql = "SELECT * FROM rooms WHERE name = '" + name + "';";
+    console.log(sql);
+    client.query(sql, [name], (err, results) => {
+      if (err) throw err;
+      cb(results.rows[0]);
+    });
+  },
+
+  find_messages: function(room_id, cb) {
+    var sql = 'SELECT * FROM messages WHERE room_id = $1';
+    client.query(sql, [room_id], (err, results) => {
+      if (err) throw err;
+      cb(results.rows);
+    });
+  },
+
+  generate_aes_key: function() {
+    return crypto.randomBytes(32).toString('hex');
+  }
+
 }
--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 require('dotenv').config();
-require("./db.js");
+var dbfun    = require("./db.js");
 
 var fs       = require("fs");
 var express  = require('express');
@@ -8,9 +8,8 @@ var server   = require('http').createServer(app);
 var io       = require('socket.io')(server);
 var rsa      = require('react-native-rsa');
 
-var key_json = require("./private_key.json");
-var key      = rsa.setPrivateEx(key_json.n, key_json.e, key_json.d, key_json.p, 
-                 key_json.q, key_json.dmp1, key_json.dmq1, key_json.coeff);
+var key_json = require("./bin/generate_key");
+//var key      = rsa.setPrivateEx(key_json.n, key_json.e, key_json.d, key_json.p, key_json.q, key_json.dmp1, key_json.dmq1, key_json.coeff);
 
 server.listen(5000, () => console.log('Server listening at port 5000'));
 
@@ -24,19 +23,20 @@ io.on('connection', (socket) => {
 function join_room(encrypted_data) {
   // Decrypt with server RSA private key
   var unencrypted_data = encrypted_data;
-  socket.join(unencrypted_data.room_id);
+  dbfun.find_room(unencrypted_data, (room) => {
+    socket.join(room);
+  });
 }
 
 function new_message(message) {
   console.log(message);
-  create_message(message, (msg) => {
+  dbfun.create_message(message, (msg) => {
     socket.broadcast.to(msg.room_id).emit('new message', msg);
   });
 }
 
 function previous_messages(room_id) {
-  find_messages(room_id, (messages) => {
-    socket.emit("previous messages", messages); 
+  dbfun.find_messages(room_id, (messages) => {
+    socket.emit("previous messages", messages);
   });
 }
-
